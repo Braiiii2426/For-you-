607b4f562ba78488b243649e98a449f5a1f76c64
Cnxiiix <brynellebalcita23@gmail.com>
2026-08-31 19:15:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
+const openButton = document.getElementById("openButton");
 
+const welcome = document.getElementById("welcome");
+
+const messageScreen =
+    document.getElementById("messageScreen");
+
+const message =
+    document.getElementById("message");
+
+
+openButton.addEventListener("click", function () {
+
+    welcome.classList.add("hidden");
+
+    messageScreen.classList.remove("hidden");
+
+    showMessage();
+
+});
+
+
+function showMessage() {
+
+    const url =
+        new URLSearchParams(window.location.search);
+
+    const customMessage =
+        url.get("message");
+
+
+    if (!customMessage) {
+
+        message.textContent =
+            "This is where your message will appear.";
+
+        return;
+    }
+
+
+    message.textContent = "";
+
+    let position = 0;
+
+
+    function typeMessage() {
+
+        if (position < customMessage.length) {
+
+            message.textContent +=
+                customMessage[position];
+
+            position++;
+
+            setTimeout(typeMessage, 40);
+        }
+    }
+
+
+    typeMessage();
+}
